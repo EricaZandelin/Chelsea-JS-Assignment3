bf73387edb942f2a2d6e7f05bbba98383c73324d
EricaZandelin <ericazandelin93@gmail.com>
2021-05-18 17:22:44 +0200

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+# Vanilla Js and React
+
+Grundiden är samma i båda, men de har tagits fram på lite olika sätt. Då dom ser lite annorlunda ut från varandra.
+Men försökt göra dom så lika som möjligt :-). 
--- app.js
+++ app.js
@@ -61,3 +61,18 @@ function yesButton() {
     }
     document.getElementById("demo").innerHTML = txt;
   }
+
+
+  /* MODAL */
+
+  const open = document.getElementById('open');
+  const modal__container = document.getElementById('modal__container');
+  const close = document.getElementById('close-modal-btn');
+
+open.addEventListener('click', () => {
+  modal__container.classList.add('show');
+})
+
+close.addEventListener('click', () => {
+  modal__container.classList.remove('show');
+})
